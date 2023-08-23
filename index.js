d40df0e5d607ded2a03843438acb4462d1061963
sayantan1821 <ksayantan1823@gmail.com>
2023-08-23 22:05:26 +0530

    add default api message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const auth = require("./routes/api/auth");
 const products = require("./routes/api/products");
 const cart = require("./routes/api/cart");
-const order = require("./routes/api/order")
+const order = require("./routes/api/order");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 const importData = require("./Database/importData");
@@ -16,9 +16,9 @@ app.use("/api/auth", auth);
 app.use("/api/products", products);
 app.use("/api/cart", cart);
 app.use("/api/order", order);
-// app.use("/", (req, res) => {
-//   res.send("API is running");
-// });
+app.use("/", (req, res) => {
+  res.send("API is running");
+});
 let PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("server is running on " + PORT);
