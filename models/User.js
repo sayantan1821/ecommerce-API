d6ba25dccae7d4a6ce6475b68cdfb8866ce917ae
sayantan1821 <ksayantan1823@gmail.com>
2023-08-23 19:32:40 +0530

    ädd cart api

--- models/User.js
+++ models/User.js
@@ -22,9 +22,15 @@ const UserSchema = new mongoose.Schema({
   // ],
   cart: [
     {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: "product" },
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "product" },
       quantity: {
         type: Number,
+        required: true,
+        min: [1, "Quantity can not be less then 1."],
+      },
+      price: {
+        type: Number,
+        required: true,
       },
     },
   ],
--- routes/api/cart.js
+++ routes/api/cart.js
@@ -1,60 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
 
-const User = require("../../models/User")
-const Product = require("../../models/Product")
-router.get(
-  '/getUserCartDetails',
-  auth,
-  async (req, res) => {
-    try {
-      let cartItems = await User.findOne({_id : req.user._id}).cart;
-
-      if (cartItems) {
-        return res.json(cartItems);
-      }
-      res.status(404).send('No cartItems Found');
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
+const User = require("../../models/User");
+const Product = require("../../models/Product");
+router.get("/getUserCartDetails", auth, async (req, res) => {
+  try {
+    let user = await User.findOne({ _id: req.user.id });
+    if (user) {
+      return res.json(user.cart);
     }
+    res.status(404).send("No Items Found");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
   }
-);
-// router.get(
-//   '/getProducts',
-//   auth,
-//   async (req, res) => {
-//     try {
-//       let products = await Product.find({});
+});
 
-//       if (products) {
-//         return res.json(products);
-//       }
-//       res.status(404).send('No products Found');
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send('Server error');
-//     }
-//   }
-// );
+router.post("/addToCart", auth, async (req, res) => {
+  const { productId, quantity } = req.body;
+  try {
+    let product = await Product.findOne({ _id: req.body.productId });
+    let user = await User.findOne({ _id: req.user.id }).select('-password');
+    if (user) {
+      let itemIndex = user.cart.findIndex((p) => p.productId == productId);
+      if (itemIndex > -1) {
+        let productItem = user.cart[itemIndex];
+        productItem.quantity = quantity;
+        productItem.price = product.price;
+        user.cart[itemIndex] = productItem;
+      } else {
+        user.cart.push({ productId, quantity: quantity, price: product.price });
+      }
+      user.updateOne({ _id: req.user._id }, { $set: user.cart });
+      let result = await user.save();
+      return res.status(201).json(result);
+    }
+    res.status(404).send("User Not Found");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
-// router.get(
-//   '/getProductById',
-//   auth,
-//   async (req, res) => {
-//     try {
-//       let product = await Product.find(req.body);
+router.get("/removeFromCart", auth, async (req, res) => {
+  try {
+    let user = await User.findOne({ _id: req.user.id }).select("-password");
 
-//       if (product) {
-//         return res.json(product);
-//       }
-//       res.status(404).send('No product Found');
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send('Server error');
-//     }
-//   }
-// );
+    if (user) {
+      let itemIndex = user.cart.findIndex(
+        (p) => p.productId == req.body.productId
+      );
+      if (itemIndex > -1) {
+        user.cart.splice(itemIndex, 1);
+        user.updateOne({ _id: req.user._id }, { $set: user.cart });
+        let result = await user.save();
+        return res.status(201).json(result);
+      } else {
+        res.status(404).send("Product is missing in cart");
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
